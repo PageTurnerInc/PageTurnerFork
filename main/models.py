ba74baa0f9aa64fd934fe5a6f0e212d243f53373
briliant22 <briliantnajmi@gmail.com>
2023-10-24 04:57:39 +0700

    main app for user account, login, authentication

---
+++ main/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    is_premium = models.BooleanField()
+
+    def __str__(self):
+        return self.user.username
---
+++ main/urls.py
@@ -0,0 +1,4 @@
+from django.urls import path
+# from main.views import 
+
+app_name = 'main'
