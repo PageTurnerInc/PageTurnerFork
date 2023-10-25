c5f28f099040d9a9a5e7f8f4122e1e216b20e3fb
briliant22 <briliantnajmi@gmail.com>
2023-10-25 14:20:23 +0700

    update main app

--- main/models.py
+++ main/models.py
@@ -6,7 +6,13 @@ class Account(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
-    is_premium = models.BooleanField()
+    is_premium = models.CharField(max_length=3)
+
+    def get_is_premium(self):
+        if self.is_premium == "Yes":
+            return True
+        elif self.is_premium == "No":
+            return False
 
     def __str__(self):
         return self.user.username
--- main/views.py
+++ main/views.py
@@ -1,3 +1,60 @@
+from main.models import Account
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.urls import reverse
+from django.shortcuts import redirect
+from main.forms import CreateAccountForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required(login_url='/login')
+def index(request):
+    account = Account.objects.filter(user=request.user)
+
+    context = {
+        'user': request.user.username,
+        'account': account,
+    }
+
+    return render(request, 'index.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:index"))
+            return response
+        else:
+            messages.info(request, 'Invalid username/password!')
+            
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = redirect('main:index')
+    return response
+
+def create_account(request):
+    form = CreateAccountForm()
+
+    if request.method == "POST":
+        form = CreateAccountForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            Account.objects.create(
+                user = new_user,
+                full_name = new_user.full_name,
+                email = new_user.email,
+                is_premium = new_user.is_premium,
+                )
+            messages.success(request, 'Sign up successful!')
+            return redirect('main:login_user')
+    
+    context = {'form':form}
+    return render(request, 'create_account.html', context)
