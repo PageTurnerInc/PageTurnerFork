bb7dcb368e18b44be2da210ebe50285acc8ba01b
Austin <austinsusanto@gmail.com>
2023-10-25 19:37:43 +0700

    Update: imported books data

---
+++ book/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from isbn_field import ISBNField
+
+class Book(models.Model):
+    isbn = ISBNField()
+    book_title = models.TextField(null=True, blank=True)
+    book_author = models.TextField(null=True, blank=True)
+    year_of_publication = models.IntegerField(null=True, blank=True)
+    publisher = models.TextField(null=True, blank=True)
+    image_url_s = models.TextField(null=True, blank=True)
+    image_url_m = models.TextField(null=True, blank=True)
+    image_url_l = models.TextField(null=True, blank=True)
