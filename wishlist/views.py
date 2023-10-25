5ad6c981f39813345160616ef229418c3ac31488
dindakiranaa <dindakirana104@gmail.com>
2023-10-26 06:38:57 +0700

    update

--- wishlist/views.py
+++ wishlist/views.py
@@ -50,38 +50,38 @@ def add_to_wishlist(request, book_id):
         
 
         
-def register(request):
-    form = UserCreationForm()
+# def register(request):
+#     form = UserCreationForm()
 
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Your account has been successfully created!')
-            return redirect('wishlist:login')
-    context = {'form':form}
-    return render(request, 'register.html', context)
+#     if request.method == "POST":
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, 'Your account has been successfully created!')
+#             return redirect('wishlist:login')
+#     context = {'form':form}
+#     return render(request, 'register.html', context)
 
-def login_user(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            response = HttpResponseRedirect(reverse("wishlist:show_main")) 
-            response.set_cookie('last_login', str(datetime.datetime.now()))
-            return response
-        else:
-            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
-    context = {}
-    return render(request, 'login.html', context)
+# def login_user(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             login(request, user)
+#             response = HttpResponseRedirect(reverse("wishlist:show_main")) 
+#             response.set_cookie('last_login', str(datetime.datetime.now()))
+#             return response
+#         else:
+#             messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+#     context = {}
+#     return render(request, 'login.html', context)
 
-def logout_user(request):
-    logout(request)
-    response = HttpResponseRedirect(reverse('wishlist:login'))
-    response.delete_cookie('last_login')
-    return response
+# def logout_user(request):
+#     logout(request)
+#     response = HttpResponseRedirect(reverse('wishlist:login'))
+#     response.delete_cookie('last_login')
+#     return response
 
 @login_required(login_url='/login')
 def delete_book(request, book_id):
