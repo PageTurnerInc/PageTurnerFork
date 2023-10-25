9343f47b35cfadea3443bacc72ce526e0956a3c9
dindakiranaa <dindakirana104@gmail.com>
2023-10-26 06:24:51 +0700

    update

--- page_turner/urls.py
+++ page_turner/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('wishlist.urls')),
+    path('wishlist/', include('wishlist.urls')),
     path('', include('main.urls')),
 ]
