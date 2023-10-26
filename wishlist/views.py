14294986a232e33730757b8df02f718f434b8693
dindakiranaa <dindakirana104@gmail.com>
2023-10-26 19:47:54 +0700

    update wishlist

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,17 +1,16 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect, render
 from .models import Book, Wishlist
-from django.contrib.auth import authenticate, login 
-from django.contrib.auth import logout
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
-from django.http import HttpResponseNotFound, HttpResponseRedirect
+from main.models import Account  # Impor model Account dari modul "main"
+from django.contrib.auth import login
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound, JsonResponse
 from django.urls import reverse
-import datetime
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
 
-@login_required(login_url='/login')
+
+@login_required(login_url='main:login_user')
 def show_main(request):
     # Ambil semua buku yang tersedia
     available_books = Book.objects.all()
@@ -22,22 +21,22 @@ def show_main(request):
 
     return render(request, 'main.html', context)
 
-@login_required(login_url='/login')
+@login_required(login_url='main:login_user')
 def show_wishlist(request):
     user = request.user
     wishlists = Wishlist.objects.filter(user=user)
     books = [wishlist.books for wishlist in wishlists]
 
     context = {
-        'wishlist': books,  # Mengirim daftar buku ke template
+        'books': books,  # Mengirim daftar buku ke template
         'user': user
     }
     return render(request, 'wishlist.html', context)
 
-@login_required(login_url='/login')
+@login_required(login_url='main:login_user')
 def add_to_wishlist(request, book_id):
-    if request.method == "GET":
-        return HttpResponseRedirect(reverse("wishlist:show_main"))
+    # if request.method == "GET":
+    #     return HttpResponseRedirect(reverse("wishlist:show_main"))
     if request.method == "POST":
         books = Book.objects.get(id=book_id)
         try:
@@ -47,43 +46,9 @@ def add_to_wishlist(request, book_id):
         except Wishlist.DoesNotExist:
             Wishlist.objects.create(user=request.user, books=books)
         return HttpResponseRedirect(reverse("wishlist:show_wishlist"))
-        
 
-        
-# def register(request):
-#     form = UserCreationForm()
-
-#     if request.method == "POST":
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             messages.success(request, 'Your account has been successfully created!')
-#             return redirect('wishlist:login')
-#     context = {'form':form}
-#     return render(request, 'register.html', context)
-
-# def login_user(request):
-#     if request.method == 'POST':
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-#         user = authenticate(request, username=username, password=password)
-#         if user is not None:
-#             login(request, user)
-#             response = HttpResponseRedirect(reverse("wishlist:show_main")) 
-#             response.set_cookie('last_login', str(datetime.datetime.now()))
-#             return response
-#         else:
-#             messages.info(request, 'Sorry, incorrect username or password. Please try again.')
-#     context = {}
-#     return render(request, 'login.html', context)
-
-# def logout_user(request):
-#     logout(request)
-#     response = HttpResponseRedirect(reverse('wishlist:login'))
-#     response.delete_cookie('last_login')
-#     return response
-
-@login_required(login_url='/login')
+@csrf_exempt 
+@login_required(login_url='main:login_user')
 def delete_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     
@@ -96,3 +61,11 @@ def delete_book(request, book_id):
         messages.error(request, 'Book not found in your wishlist')
 
     return HttpResponseRedirect(reverse("wishlist:show_wishlist"))
+
+def get_wishlist_items(request):
+    user = request.user
+    wishlists = Wishlist.objects.filter(user=user)
+    books = [wishlist.books for wishlist in wishlists]
+    # Mengambil data yang sesuai dengan atribut yang Anda akses di JavaScript
+    serialized_books = serializers.serialize('json', books, fields=('image_url_s', 'book_title', 'book_author', 'year_of_publication', 'publisher'))
+    return HttpResponse(serialized_books, content_type="application/json")
