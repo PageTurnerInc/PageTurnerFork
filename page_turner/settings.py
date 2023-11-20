b5cccc4ada13ad4930317f75643ec12272d3ce4d
DimasHN_04 <dimashn04@gmail.com>
2023-11-20 19:01:21 +0700

    Fix @settings.py

--- page_turner/settings.py
+++ page_turner/settings.py
@@ -17,7 +17,7 @@ import os
 from django.core.management.commands.runserver import Command as rs
 rs.default_port='8080'
 
-CSRF_TRUSTED_ORIGINS = ['http://pageturner-c06-tk.pbp.cs.ui.ac.id', 'https://pageturner-c06-tk.pbp.cs.ui.ac.id/']
+CSRF_TRUSTED_ORIGINS = ['http://pageturner-c06-tk.pbp.cs.ui.ac.id', 'https://pageturner-c06-tk.pbp.cs.ui.ac.id']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
