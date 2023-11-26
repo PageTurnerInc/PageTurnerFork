caa3394e642b8f678c5dc0946bcf5642e30bc9b6
DimasHN_04 <dimashn04@gmail.com>
2023-11-26 21:59:36 +0700

    Fix @authentication:views.py

--- authentication/views.py
+++ authentication/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.http import JsonResponse
 
+@csrf_exempt
 def login(request):
     username = request.POST['username']
     password = request.POST['password']
